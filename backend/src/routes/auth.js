3fe9c5c89509191ee69eeaf364813ada5b65e9ee
Jayesh Naphade <jnaphade12@gmail.com>
2026-06-09 21:56:34 +0530

    Update auth.js

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -105,6 +105,114 @@ message:
 
 }
 );
+router.post(
+"/login",
+async (req,res)=>{
 
+try{
+
+const {
+email,
+password
+}
+=
+req.body;
+
+const user =
+await getUserByEmail(
+email
+);
+
+if(!user){
+
+return res
+.status(400)
+.json({
+
+message:
+"Invalid credentials"
+
+});
+
+}
+
+const validPassword =
+await bcrypt.compare(
+
+password,
+
+user.password_hash
+
+);
+
+if(!validPassword){
+
+return res
+.status(400)
+.json({
+
+message:
+"Invalid credentials"
+
+});
+
+}
+
+const token =
+jwt.sign(
+
+{
+id:user.id,
+email:user.email
+},
+
+process.env.JWT_SECRET,
+
+{
+expiresIn:"7d"
+}
+
+);
+
+res.json({
+
+message:
+"Login successful",
+
+token,
+
+user:{
+
+id:user.id,
+
+username:
+user.username,
+
+email:
+user.email
+
+}
+
+});
+
+}
+catch(err){
+
+console.error(
+err
+);
+
+res.status(500)
+.json({
+
+message:
+"Server Error"
+
+});
+
+}
+
+}
+);
 module.exports =
 router;
