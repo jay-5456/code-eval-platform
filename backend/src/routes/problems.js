65f6aa157ce0e7370e7e3a24ea3e2318f90a1f0f
Jayesh Naphade <jnaphade12@gmail.com>
2026-06-09 13:54:55 +0530

    completed database migration

--- backend/src/routes/problems.js
+++ backend/src/routes/problems.js
@@ -1,26 +1,102 @@
-const express = require("express");
-const router = express.Router();
+const express =
+require("express");
 
-const problems = require("../data/problems");
+const router =
+express.Router();
 
-router.get("/", (req, res) => {
-  res.json(problems);
+const {
+  getAllProblems,
+  getProblemById
+} =
+require(
+"../models/problemModel"
+);
+
+router.get(
+"/",
+async (req,res)=>{
+
+try{
+
+const problems =
+await getAllProblems();
+
+res.json(
+problems
+);
+
+}
+catch(err){
+
+console.error(
+err
+);
+
+res.status(500)
+.json({
+
+message:
+"Server Error"
+
 });
 
-router.get("/:id", (req, res) => {
-  const id = Number(req.params.id);
+}
 
-  const problem = problems.find(
-    p => p.id === id
-  );
+}
+);
 
-  if (!problem) {
-    return res.status(404).json({
-      message: "Problem not found"
-    });
-  }
+router.get(
+"/:id",
+async (req,res)=>{
 
-  res.json(problem);
+try{
+
+const id =
+Number(
+req.params.id
+);
+
+const problem =
+await getProblemById(
+id
+);
+
+if(!problem){
+
+return res
+.status(404)
+.json({
+
+message:
+"Problem not found"
+
 });
 
-module.exports = router;
+}
+
+res.json(
+problem
+);
+
+}
+catch(err){
+
+console.error(
+err
+);
+
+res.status(500)
+.json({
+
+message:
+"Server Error"
+
+});
+
+}
+
+}
+);
+
+module.exports =
+router;
