d8f4c2de423c5895f1ea1ebddb55f7d5921d66c0
Jayesh Naphade <jnaphade12@gmail.com>
2026-06-09 13:51:48 +0530

    Update problemModel.js

--- backend/src/models/problemModel.js
+++ backend/src/models/problemModel.js
@@ -1,22 +1,83 @@
 const pool =
 require("../db/db");
 
-async function
-getAllProblems(){
+async function getAllProblems() {
 
-const result =
-await pool.query(
-`
-SELECT *
-FROM problems
-ORDER BY id
-`
-);
+  const result =
+    await pool.query(
+      `
+      SELECT
+      id,
+      title,
+      description,
+      difficulty
+      FROM problems
+      ORDER BY id
+      `
+    );
 
-return result.rows;
+  return result.rows;
+}
 
+async function getProblemById(id) {
+
+  const problemResult =
+    await pool.query(
+      `
+      SELECT *
+      FROM problems
+      WHERE id = $1
+      `,
+      [id]
+    );
+
+  if (
+    problemResult.rows.length === 0
+  ) {
+    return null;
+  }
+
+  const testcaseResult =
+    await pool.query(
+      `
+      SELECT *
+      FROM testcases
+      WHERE problem_id = $1
+      `,
+      [id]
+    );
+
+  const problem =
+    problemResult.rows[0];
+
+  problem.sampleTestCases =
+    testcaseResult.rows
+      .filter(
+        t => !t.is_hidden
+      )
+      .map(
+        t => ({
+          input: t.input,
+          output: t.output
+        })
+      );
+
+  problem.hiddenTestCases =
+    testcaseResult.rows
+      .filter(
+        t => t.is_hidden
+      )
+      .map(
+        t => ({
+          input: t.input,
+          output: t.output
+        })
+      );
+
+  return problem;
 }
 
 module.exports = {
-getAllProblems
+  getAllProblems,
+  getProblemById
 };
