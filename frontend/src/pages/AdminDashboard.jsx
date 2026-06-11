cf06f165aa14b739324decbb1deeb0fec65eeadf
Jayesh Naphade <jnaphade12@gmail.com>
2026-06-11 18:24:30 +0530

    Update AdminDashboard.jsx

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,4 +1,68 @@
+import { useState }
+from "react";
+
+import api
+from "../services/api";
+
 function AdminDashboard(){
+
+const [title,setTitle] =
+useState("");
+
+const [
+description,
+setDescription
+]
+=
+useState("");
+
+const [
+difficulty,
+setDifficulty
+]
+=
+useState("Easy");
+
+const createProblem =
+async () => {
+
+try{
+
+const res =
+await api.post(
+"/admin/problem",
+{
+title,
+description,
+difficulty
+}
+);
+
+alert(
+"Problem Created"
+);
+
+console.log(
+res.data
+);
+
+setTitle("");
+setDescription("");
+
+}
+catch(err){
+
+alert(
+"Failed"
+);
+
+console.log(
+err
+);
+
+}
+
+};
 
 return(
 
@@ -12,9 +76,78 @@ padding:"20px"
 Admin Dashboard
 </h1>
 
-<p>
-Coming Soon
-</p>
+<h2>
+Create Problem
+</h2>
+
+<input
+placeholder="Title"
+value={title}
+onChange={(e)=>
+setTitle(
+e.target.value
+)}
+style={{
+width:"400px"
+}}
+/>
+
+<br />
+<br />
+
+<textarea
+placeholder=
+"Description"
+value={
+description
+}
+onChange={(e)=>
+setDescription(
+e.target.value
+)}
+rows="5"
+cols="60"
+/>
+
+<br />
+<br />
+
+<select
+value={
+difficulty
+}
+onChange={(e)=>
+setDifficulty(
+e.target.value
+)}
+>
+
+<option>
+Easy
+</option>
+
+<option>
+Medium
+</option>
+
+<option>
+Hard
+</option>
+
+</select>
+
+<br />
+<br />
+
+<button
+onClick={
+createProblem
+}
+>
+
+Create Problem
+
+</button>
 
 </div>
 
