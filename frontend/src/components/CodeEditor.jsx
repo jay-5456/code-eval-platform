00ba8a03598404229d34667c1930db8b7ffc905c
Jayesh Naphade <jnaphade12@gmail.com>
2026-06-20 00:35:50 +0530

    Update CodeEditor.jsx

--- frontend/src/components/CodeEditor.jsx
+++ frontend/src/components/CodeEditor.jsx
@@ -1,21 +1,51 @@
 import Editor from "@monaco-editor/react";
 
 function CodeEditor({
-  language,
-  code,
-  setCode
+language,
+code,
+setCode
 }) {
 
-  return (
-    <Editor
-      height="500px"
-      language={language}
-      value={code}
-      onChange={(value) =>
-        setCode(value)
-      }
-    />
-  );
+return (
+
+<div
+style={{
+border:"1px solid #2a2a2a",
+borderRadius:"12px",
+overflow:"hidden",
+boxShadow:
+"0 0 15px rgba(0,0,0,0.3)"
+}}
+>
+
+<Editor
+height="600px"
+theme="vs-dark"
+language={language}
+value={code}
+onChange={(value)=>
+setCode(
+value
+)
+}
+options={{
+fontSize:16,
+minimap:{
+enabled:false
+},
+scrollBeyondLastLine:false,
+roundedSelection:true,
+padding:{
+top:20
+},
+automaticLayout:true
+}}
+/>
+
+</div>
+
+);
+
 }
 
 export default CodeEditor;
